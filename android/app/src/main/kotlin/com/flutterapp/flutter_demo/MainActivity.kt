218134b668d0d37b08e5bc6d514dec4215ad1fad
AndroidTeam <khushbut@hyperlinkinfosystem.net.in>
2020-12-05 22:53:30 +0530

    -Mainly covered widgets- MaterialApp, Scaffold, AppBar, Image, IconButton

---
+++ android/app/src/main/kotlin/com/flutterapp/flutter_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterapp.flutter_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+
+//This is the demo project to start the flutter development
+//In this project mainly covered this topics with the properties:  MaterialApp, Scaffold, AppBar, IconButton, Image Widget
+
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightBlueAccent[100],
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent[200],
+        title: Text("I Am Poor"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          hoverColor: Colors.green,
+          onPressed:(){
+
+          }
+        ),
+        brightness: Brightness.light ,
+        actions: <Widget>[
+
+          IconButton(
+            icon: Icon(Icons.attachment),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_location),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+        ),
+      ),
+    ),
+    debugShowCheckedModeBanner: false, //Removing Debug Banner
+  ));
+}
+
+
+
+
+
